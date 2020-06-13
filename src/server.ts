f5d9e64d4d0f247cb66edf8c6ba7b60e70f900db
xlsarath <xlsarathchandra@gmail.com>
2020-06-13 15:07:12 -0700

    changes to server.ts and yaml

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
+import Jimp = require('jimp');
+
 (async () => {
 
   // Init the Express application
@@ -13,32 +15,21 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   
-  app.get("/filteredimage/", async (req: Request, res: Response) => {
+  // Image filtering Endpoint
+  // Returns a filtered image to the user
+  app.get( "/filteredimage", async ( req, res ) => {
     let { image_url } = req.query;
-  //    1. validate the image_url query
+
     if (!image_url) {
-      return res.status(400).send(`image_url is required`);
+      return res.status(422).send({ auth: true, message: 'image_url is required.' });
     }
 
-    try {
-        //    2. call filterImageFromURL(image_url) to filter the image
-      const filteredpath = await filterImageFromURL(image_url)
-      
-      //    3. send the resulting file in the response
-      await res.status(200).sendFile(filteredpath, {}, (err) => {
-        if (err) { return res.status(422).send(`Not able to process the image`); }
-        // Deleting the used image file.
-          //    4. deletes any files on the server on finish of the response
-        deleteLocalFiles([filteredpath])
-      })
-    } catch (err) {
-      res.status(422).send(`Not able to process the image, Make sure image url is correct`);
-    }
-  });
-  //! END @TODO1
-  
+    let filteredPath = await filterImageFromURL(image_url);
+    res.status(200).sendFile(filteredPath, () => { deleteLocalFiles([filteredPath]); });
+  } );
+
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
